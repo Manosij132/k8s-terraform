e89bdf5cb894ad75754a56ec836f505b45d021ac
Manosij132 <ankur.majumder96@gmail.com>
2025-10-19 23:04:18 +0530

    changed sub

--- parent/provider.tf
+++ parent/provider.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.47.0"
     }
   }
   backend "azurerm" {
-    resource_group_name = "rg-tfstate-mano"
-    storage_account_name = "stgtfstatemano"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
+    resource_group_name  = "rg-tfstate-mano"
+    storage_account_name = "stgtfstatemano1"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  features{}
-  subscription_id = "a042c51a-bb9d-48f6-af09-55adf40d7a98"
+  features {}
+  subscription_id = "90ba3f99-29ea-4a71-8106-8a6775ca9f61"
 }
